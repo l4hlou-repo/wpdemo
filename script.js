8b8233bc24c9b6302627f6139100dcf7dc44d307
l4hlou-repo <58866668+l4hlou-repo@users.noreply.github.com>
2023-08-27 13:49:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-document.getElementById('calculatorForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    var gender = document.getElementById('gender').value;
-    var age = parseInt(document.getElementById('age').value);
-    var weight = parseInt(document.getElementById('weight').value);
-    var height = parseInt(document.getElementById('height').value);
-    var activityLevel = parseFloat(document.getElementById('activityLevel').value);
-    var goal = document.getElementById('goal').value;
-    
-    var bmr;
-    var tdee;
-    var calorieIntake;
-    
-    if (gender === 'male') {
-        bmr = 10 * weight + 6.25 * height - 5 * age + 5;
-    } else {
-        bmr = 10 * weight + 6.25 * height - 5 * age - 161;
-    }
-    
-    switch (activityLevel) {
+document.addEventListener('DOMContentLoaded', function () {
+    const calculateButton = document.querySelector('.btn-primary');
+  
+    calculateButton.addEventListener('click', function (event) {
+      event.preventDefault();
+  
+      const weight = parseFloat(document.querySelector('input[name="weight"]').value);
+      const height = parseFloat(document.querySelector('input[name="height"]').value);
+      const age = parseFloat(document.querySelector('input[name="age"]').value);
+      const gender = document.querySelector('select[name="gender"]').value;
+      const activityLevel = parseFloat(document.querySelector('select[name="activity"]').value);
+  
+      let bmr = 0;
+  
+      if (gender === 'male') {
+        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+      } else if (gender === 'female') {
+        bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+      }
+  
+      let calories = 0;
+  
+      switch (activityLevel) {
         case 1.2:
-            tdee = bmr * 1.2;
-            break;
+          calories = bmr * 1.2; // Sedentary
+          break;
         case 1.375:
-            tdee = bmr * 1.375;
-            break;
+          calories = bmr * 1.375; // Lightly active
+          break;
         case 1.55:
-            tdee = bmr * 1.55;
-            break;
+          calories = bmr * 1.55; // Moderately active
+          break;
         case 1.725:
-            tdee = bmr * 1.725;
-            break;
+          calories = bmr * 1.725; // Very active
+          break;
         case 1.9:
-            tdee = bmr * 1.9;
-            break;
-        default:
-            tdee = bmr;
-    }
-    
-    if (goal === 'lose') {
-        calorieIntake = tdee - 500;
-    } else if (goal === 'gain') {
-        calorieIntake = tdee + 500;
-    } else {
-        calorieIntake = tdee;
-    }
-    
-    document.getElementById('caloriesResult').textContent = 'Calories to consume per day: ' + calorieIntake.toFixed(0);
-    
-    var protein = weight * 2.2;
-    var carbohydrates = weight * 4;
-    var fat = weight * 0.4;
-    
-    document.getElementById('protein').textContent = protein.toFixed(1) + 'g';
-    document.getElementById('carbohydrates').textContent = carbohydrates.toFixed(1) + 'g';
-    document.getElementById('fat').textContent = fat.toFixed(1) + 'g';
-});
+          calories = bmr * 1.9; // Super active
+          break;
+      }
+  
+      const resultBox = document.createElement('div');
+      resultBox.className = 'user-box result-box';
+      resultBox.textContent = `Recommended Daily Calories: ${calories.toFixed(2)}`;
+  
+      const form = document.querySelector('form');
+      form.appendChild(resultBox);
+    });
+  });
+  
